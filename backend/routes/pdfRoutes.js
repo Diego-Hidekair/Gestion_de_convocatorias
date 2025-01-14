c4128a7e298673e206a9799e96b0ce770b28dfa0
Diego_Fajardo <diego7876123@gmail.com>
2025-01-14 18:34:27 -0400

    no se avanzo

--- backend/routes/pdfRoutes.js
+++ backend/routes/pdfRoutes.js
@@ -2,16 +2,13 @@
 const express = require('express');
 const router = express.Router();
 const pdfController = require('../controllers/pdfController');
+console.log('Funciones del controlador:', pdfController);
+
 const { authenticateToken, verificarRolSecretaria } = require('../middleware/authMiddleware');
 
 router.get('/generar/:id_convocatoria/:id_honorario', authenticateToken, verificarRolSecretaria, pdfController.generatePDF);
-//router.get('/combinado/:id_convocatoria', pdfController.viewCombinedPDF); 
 router.get( '/combinado/ver/:id_convocatoria', authenticateToken, pdfController.viewCombinedPDF );
-
-//router.get('/download/:id_convocatoria', authenticateToken, pdfController.downloadCombinedPDF); 
 router.get('/descargar/:id_convocatoria', pdfController.downloadCombinedPDF);
-//router.get('/combinado/descargar/:id_convocatoria', authenticateToken, pdfController.downloadCombinedPDF);
-
 router.delete( '/eliminar/:id_convocatoria', authenticateToken, verificarRolSecretaria, pdfController.deletePDF);
 
 
--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -2,14 +2,20 @@
 const express = require('express');
 const router = express.Router();
 const { createUser, upload, getUsuarios, deleteUser, updateUser, getCurrentUser, getUsuarioById } = require('../controllers/usuarioController');
-const { authenticateToken, authorizeAdmin } = require('../middleware/authMiddleware');
+//const { authenticateToken, authorizeAdmin } = require('../middleware/authMiddleware');
+const { authenticateToken, authorizeAdmin, verificarRol } = require('../middleware/authMiddleware');
+
 
 router.get('/me', authenticateToken, getCurrentUser);
 router.get('/', authenticateToken, authorizeAdmin, getUsuarios);
 router.get('/:id_usuario', authenticateToken, getUsuarioById);
 //router.post('/', authenticateToken, authorizeAdmin, createUser);
-router.post('/', upload.single('foto_perfil'), authenticateToken, authorizeAdmin, createUser);
+//router.post('/', upload.single('foto_perfil'), authenticateToken, authorizeAdmin, createUser);
+router.post('/', upload.single('foto_perfil'), authenticateToken, verificarRol('admin', 'secretaria'), createUser);
+
 router.delete('/:id_usuario', authenticateToken, authorizeAdmin, deleteUser);
 router.put('/:id_usuario', authenticateToken, authorizeAdmin, updateUser);
 
 module.exports = router;
+
+
